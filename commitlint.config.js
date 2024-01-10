467dea5d33fd74d8a7f40e07ade35f894dc19ad2
Kazuki <96904689+Kazuki123-k@users.noreply.github.com>
2024-01-10 20:02:43 +0900

    chore: fix yaml file

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,5 +2,8 @@ module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
     "header-max-length": [2, "always", 300],
+    "header-min-length": [2, always, 1],
+    "subject-max-length": [2, always, 50],
+    "subject-min-length": [2, always, 1],
   },
 };
