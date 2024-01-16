73862839e6f040b0683db69eefc908bed8fd5c1e
Kazuki <96904689+Kazuki123-k@users.noreply.github.com>
2024-01-16 21:10:14 +0900

    chore: place api key in env variable

---
+++ frontend/my-app/config/firebase.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { FIREBASE_APIKEY } from "@env";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: FIREBASE_APIKEY,
+  authDomain: "favors-73102.firebaseapp.com",
+  projectId: "favors-73102",
+  storageBucket: "favors-73102.appspot.com",
+  messagingSenderId: "612302916982",
+  appId: "1:612302916982:web:0c4b7694696138c277be57",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
---
+++ frontend/my-app/pages/Authentication.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Login from "./Login";
+import Signup from "./Signup";
+import { View } from "react-native";
+
+const Authentication = () => {
+  return (
+    <>
+      <Login />
+      <Signup />
+    </>
+  );
+};
+
+export default Authentication;
