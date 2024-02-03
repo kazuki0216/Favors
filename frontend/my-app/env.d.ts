1ff66d10b772af7ecacff88b66f5cc451f8ad3f0
Kazuki <96904689+Kazuki123-k@users.noreply.github.com>
2024-02-03 11:54:14 +0900

    chore: currently implementing profile page and integrating paypay payment api

--- frontend/my-app/env.d.ts
+++ frontend/my-app/env.d.ts
@@ -3,4 +3,7 @@ declare module "@env" {
   export const APP_MAJOR_VERSION: string;
   export const APP_MINOR_VERSION: string;
   export const APP_PATCH_VERSION: string;
+  export const PAYPAY_API_KEY: string;
+  export const PAYPAY_API_SECRET: string;
+  export const PAYPAY_MERCHANT_ID: string;
 }
