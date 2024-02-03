599398f1df314b5b3afc3db665e8b5e7440803b1
Kazuki <96904689+Kazuki123-k@users.noreply.github.com>
2024-02-03 12:45:29 +0900

    wip: working on env config with firebase

--- frontend/my-app/types/env.d.ts
+++ frontend/my-app/types/env.d.ts
@@ -1,9 +1,9 @@
 declare module "@env" {
   export const FIREBASE_APIKEY: string;
+  export const API_KEY: string;
+  export const API_SECRET: string;
+  export const MERCHANT_ID: string;
   export const APP_MAJOR_VERSION: string;
   export const APP_MINOR_VERSION: string;
   export const APP_PATCH_VERSION: string;
-  export const PAYPAY_API_KEY: string;
-  export const PAYPAY_API_SECRET: string;
-  export const PAYPAY_MERCHANT_ID: string;
 }
